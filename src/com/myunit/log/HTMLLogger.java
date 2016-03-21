167e6b36a5675d97387f8795b09966f1ea723633
Michele Ferraro <micfer96@gmail.com>
2016-03-21 16:36:54 +0100

    Abstracted HTMLLogger into FileLogger, used through Composition

--- src/com/myunit/log/HTMLLogger.java
+++ src/com/myunit/log/HTMLLogger.java
@@ -6,66 +6,54 @@ import java.io.PrintStream;
 import java.lang.reflect.Method;
 
 public class HTMLLogger implements Logger {
-    private PrintStream stream;
-    private String logFile;
-    private String extraLogs;
-    private boolean autoOpenLog;
-    private boolean writeEventLog;
+    private FileLogger fileLogger;
 
     public HTMLLogger(String file) {
-        try {
-            stream = new PrintStream(file);
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: Cannot open log file");
-        }
-        logFile = file;
-        extraLogs = "";
-        autoOpenLog = true;
-        writeEventLog = false;
-        stream.print("<html><head></head><body>");
+        fileLogger = new FileLogger(file);
+        fileLogger.log("<html><head></head><body>");
     }
 
     public HTMLLogger openLogAfterTests(boolean autoOpenLog) {
-        this.autoOpenLog = autoOpenLog;
+        fileLogger.openLogAfterTests(autoOpenLog);
         return this;
     }
 
     public HTMLLogger writeEventLog(boolean writeEventLog) {
-        this.writeEventLog = writeEventLog;
+        fileLogger.writeEventLog(writeEventLog);
         return this;
     }
 
     @Override
     public void log(String message) {
-        extraLogs += message + "<br />";
+        fileLogger.logEvent(message + "<br />");
     }
 
     @Override
     public void logExceptionRaised(Class testClass, Method method, Throwable errorCause) {
-        extraLogs += testClass.getName() + "#" + method.getName() +
+        fileLogger.logEvent(testClass.getName() + "#" + method.getName() +
                 "raised an exception of type " + errorCause.getClass().getName() +
                 (errorCause.getMessage() != null ? "with message: " + errorCause.getMessage() : "") +
-                "<br />";
+                "<br />");
     }
 
     @Override
     public void logExecutingMethod(Method method) {
-        stream.print("<tr><td>"+method.getName()+"</td>");
+        fileLogger.log("<tr><td>"+method.getName()+"</td>");
     }
 
     @Override
     public void logTestCaseSuccess() {
-        stream.print("<td><font color=\"green\">Success</font></td><td></td></tr>\n");
+        fileLogger.log("<td><font color=\"green\">Success</font></td><td></td></tr>\n");
     }
 
     @Override
     public void logTestCaseFail(Throwable throwable) {
-        stream.print("<td><font color=\"red\">Fail</font></td><td>"+throwable.getClass().getName()+"</td>");
+        fileLogger.log("<td><font color=\"red\">Fail</font></td><td>"+throwable.getClass().getName()+"</td>");
         String message = throwable.getMessage();
         if (message != null) {
-            stream.print("<td>" + message + "</td>");
+            fileLogger.log("<td>" + message + "</td>");
         }
-        stream.print("</tr>\n");
+        fileLogger.log("</tr>\n");
     }
 
     @Override
@@ -75,30 +63,20 @@ public class HTMLLogger implements Logger {
 
     @Override
     public void logTestBegin(Class testClass) {
-        stream.print("<table>\n<tr><td colspan=\"3\">"+testClass.getName()+"</td></tr>\n");
+        fileLogger.log("<table>\n<tr><td colspan=\"4\">"+testClass.getName()+"</td></tr>\n");
     }
 
     @Override
     public void logTestEnd() {
-        stream.print("</table><br />\n");
+        fileLogger.log("</table><br />\n");
     }
 
     @Override
     public void logSuiteResults(int passedTests, int failedTests) {
-        stream.print("</body></html>\n\n");
-        if (writeEventLog) {
-            stream.print("Logs:<br /><br />\n" + extraLogs + "<br />\n");
-        }
-        stream.print("Executed tests: " + (passedTests+failedTests) + "<br />\n"
+        fileLogger.log("</body></html>\n\n");
+        fileLogger.log("Executed tests: " + (passedTests+failedTests) + "<br />\n"
                 + "Passed: " + passedTests + "<br />\n"
                 + "Failed: " + failedTests + "<br />\n");
-        stream.close();
-        if (autoOpenLog) {
-            try {
-                Runtime.getRuntime().exec("cmd.exe /c " + logFile);
-            } catch (IOException exception) {
-                System.out.println("Error: Cannot open log file.");
-            }
-        }
+        fileLogger.end();
     }
 }
