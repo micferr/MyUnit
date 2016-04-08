3c1adbc4a56712df600b785733dd8bd6bc468495
Michele Ferraro <micfer96@gmail.com>
2016-04-08 21:02:32 +0200

    Added Javadocs generation tasks - Started writing Javadocs

--- src/com/myunit/selftest/SelfTestSuite.java
+++ src/com/myunit/selftest/SelfTestSuite.java
@@ -3,7 +3,21 @@ package com.myunit.selftest;
 import com.myunit.log.JUnitXMLLogger;
 import com.myunit.test.*;
 
+/**
+ * The class in charge of the framework self-testing. Executing its main method starts the testing.
+ * When the testing is finished, a report is saved in JUnit XML format, such as to be compatible
+ * with test reporting frameworks such as Jenkins
+ *
+ * @see TestRunner
+ * @see com.myunit.log.Logger
+ */
 public class SelfTestSuite {
+
+    /**
+     * The main method to execute to start testing
+     *
+     * @param args      testing params, currently unused
+     */
     public static void main(String[] args){
         new TestRunner(
                 new JUnitXMLLogger("log.xml")
