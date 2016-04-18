73c09c502789efecdc252a76133e808d6fd74bab
Michele Ferraro <micfer96@gmail.com>
2016-04-18 15:58:02 +0200

    Added more comments

--- src/com/myunit/test/After.java
+++ src/com/myunit/test/After.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks methods to be executed after all the tests have run
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface After {}
